4fd1ecebf4476bcea57e058220069555aad65302
LeeHyesooo <gptn8678@gmail.com>
2025-11-09 14:44:23 +0900

    fast api 반영 - 팝업 수정

--- moodify/moodify/src/main/java/com/example/moodify/controller/ViewController.java
+++ moodify/moodify/src/main/java/com/example/moodify/controller/ViewController.java
@@ -23,6 +23,7 @@ public class ViewController { //화면 표시용
     private final TestService testService;
     private final IeumService service;
 
+    // 홈
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("candidateEmotions", Constant.EMOTIONS);
@@ -30,34 +31,52 @@ public class ViewController { //화면 표시용
         System.out.println("log");
         return "home";
     }
-/*
-    @GetMapping("/recommendations")
-    public String recommendations(Model model) throws IOException {
-        List<SongDTO> songs = testService.read().getSongs();
-        System.out.println(songs.toString());
-        model.addAttribute("songs", songs);
 
-
-        return "result"; // 추천 결과 화면 템플릿
-    }*/
-
+    // 사용자 입력값 상황, 분석 여부 체크
     @GetMapping("/check/recommendations")
-    public ResponseEntity<Map<String, String>> checkRecommendations(@RequestParam String text) throws IOException {
+    public ResponseEntity<Map<String, Object>> checkRecommendations(@RequestParam String text) throws IOException {
         System.out.println("호출 테스트");
-        // 아래 코드 -> 서비스로 뺴야됨
-//        List<SongDTO> songs = testService.read().getSongs(); // 추후에 text로 변경해야됨
-        boolean hasEmotion = false;
-        boolean hasSituation = false;
-
-
-        Map<String, String> resp = new HashMap<>();
+        RequestDTO requestDTO = new RequestDTO();
+        requestDTO.setText(text);
+        ResponseDTO response = service.getAnalysisResultAndSongs(requestDTO);
+        boolean hasEmotion = (null != response.getEmotions()) ? true : false;
+        boolean hasSituation = response.isSelection();
+        Map<String, Object> resp = new HashMap<>();
         resp.put("emotion", hasEmotion ? "Y" : "N");
+        resp.put("emotions", response.getEmotions());
         resp.put("situation", hasSituation ? "Y" : "N");
         return ResponseEntity.ok(resp);
     }
 
+
+    // 팝업 -> 선택하여 재추천
+    @PostMapping("/recommend") // <목록에 없는 상황이 입력된 경우> 선택한 상황에 따라 추천
+    public String postRecommendations(
+            @RequestParam(value = "text", required = true) String text,
+            @RequestParam(value = "emotion", required = true) List<String> emotions, // 감정 선택한 경우
+            @RequestParam(value = "situation", required = true) List<String> situations, // 상황 선택한 경우
+            Model model) throws IOException {
+
+        RequestDTO requestDTO = new RequestDTO();
+        requestDTO.setText(text);
+        requestDTO.setEmotions(emotions);
+        requestDTO.setSituations(situations);
+
+        List<SongDTO> songs = service.getSongs(requestDTO).getSongs();
+        model.addAttribute("songs", songs);
+        model.addAttribute("inputText", text);
+        model.addAttribute("emotions", emotions);
+        model.addAttribute("situations", situations);
+
+        return "result";
+
+    }
+
+
+
+    // 추천 결과
     @PostMapping("analyze-and-recommend") // 처음 입력 시 분석 + 추천(목록에 있는 감정/상황일 경우)
-    public String analysisAndRecommendations(@RequestParam(value = "text", required = false) String text, Model model) throws IOException{
+    public String analysisAndRecommendations(@RequestParam(value = "text", required = true) String text, Model model) throws IOException{
         RequestDTO requestDTO = new RequestDTO();
         requestDTO.setText(text);
 
@@ -76,16 +95,6 @@ public class ViewController { //화면 표시용
             List<String> emotions = response.getEmotions();
             List<String> situations = response.getSituations();
 
-            /*
-            Set<String> uniqueEmotions = getUniqueEmotions(songs);
-            Set<String> activeSituation = getActiveSituation(songs);
-            System.out.println("uniqueEmotions : " + uniqueEmotions.toString());
-            System.out.println("activeSituation : " + activeSituation.toString());
-
-            model.addAttribute("emotions", uniqueEmotions);
-            model.addAttribute("situations", activeSituation);
-            */
-
             model.addAttribute("songs", songs);
             model.addAttribute("inputText", text);
             model.addAttribute("emotions", emotions);
@@ -96,49 +105,6 @@ public class ViewController { //화면 표시용
 
     }
 
-    @PostMapping("/recommend") // <목록에 없는 상황이 입력된 경우> 선택한 상황에 따라 추천
-    public String postRecommendations(
-            @RequestParam(value = "text", required = false) String text,
-            @RequestParam(value = "emotion", required = false) List<String> emotions, // 감정 선택한 경우
-            @RequestParam(value = "situation", required = false) List<String> situations, // 상황 선택한 경우
-            Model model) throws IOException {
-
-        RequestDTO requestDTO = new RequestDTO();
-        requestDTO.setText(text);
-        requestDTO.setEmotions(emotions);
-        requestDTO.setSituations(situations);
-
-        List<SongDTO> songs = service.getSongs(requestDTO).getSongs();
-        model.addAttribute("songs", songs);
-        model.addAttribute("inputText", text);
-        model.addAttribute("emotions", emotions);
-        model.addAttribute("situations", situations);
-
-
-        /*
-        System.out.println("text : " + text);
-        System.out.println("emotion : " + emotion);
-        System.out.println("situation : " + situation);
-
-        System.out.println("text : " + text);
-        System.out.println("emotion : " + emotion);
-        System.out.println("situation : " + situation);
-        List<SongDTO> songs = testService.read().getSongs();
-        Set<String> uniqueEmotions = getUniqueEmotions(songs);
-        Set<String> activeSituation = getActiveSituation(songs);
-        System.out.println("uniqueEmotions : " +uniqueEmotions.toString());
-        System.out.println("activeSituation : " +activeSituation.toString());
-
-
-        model.addAttribute("emotions", uniqueEmotions);
-        model.addAttribute("situations", activeSituation);
-        model.addAttribute("songs", songs);
-        model.addAttribute("inputText",text);
-        */
-
-        return "result";
-
-    }
 
     /**
      * 감정 중복제거
